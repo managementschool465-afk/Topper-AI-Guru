a378f77a9b58774f5f2c2398f3a7c95dea962ff5
managementschool465-afk <managementschool465@gmail.com>
2026-04-21 09:46:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Root Element Mounting
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
